ffc66932416fb435b3d441c7c96875730f90699e
Selvi <74725778+miofri@users.noreply.github.com>
2022-01-20 18:04:07 +0200

    Update get_next_line.c

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -12,6 +12,18 @@
 
 #include "get_next_line.h"
 
+/* ft_strdup: returns a ptr to a new string duplicate of string s1. (malloc)
+** ft_strsub: returns a substr (malloc) from and to the given indexes.
+** ft_strchr: returns a ptr to the first occurence of the given char.
+** ft_strjoin: returns a fresh null-terminated s1 + s2. (malloc)
+**
+** handle_line will find where \n or \0 is.
+** When \n is found, we assign the line starting from 0 to where \n was found.
+** strsub will only go until i - 1. We will replace current bitem[fd] with the
+** string after \n. if the first char of bitem[fd] is \0, EOF has been reached
+** and bitem[fd] is not needed anymore, thus freed.
+** When \0 is found, we can assign the whole string to line since there is no
+** \n and free bitem[fd] as the EOF has been reached. */
 int	handle_line(const int fd, char **line, char **bitem)
 {
 	int		i;
@@ -41,6 +53,8 @@ int	handle_line(const int fd, char **line, char **bitem)
 	return (1);
 }
 
+/* checks if read() has finished reading the file, if reading failed or
+** else. */
 int	check_(const int fd, char **line, int ret, char **bitem)
 {
 	if (ret == 0 && bitem[fd] == NULL)
@@ -51,6 +65,14 @@ int	check_(const int fd, char **line, int ret, char **bitem)
 		return (handle_line(fd, line, bitem));
 }
 
+/* this function will get the no. of bytes in the file and reads the file into
+** buf from start to either EOF or \n. We're joining BUFF_SIZE at a time
+** to bitem[fd] by replacing the current bitem[fd] with bitem[fd] + buf, which
+** requires freeing bitem[fd] every time to avoid leaks before reassigning it.
+** When EOF/\n is found, we move forward to func check_.
+** static char is used to so that the 2D array remembers what was stored in it
+** between calls. It is also used to keep track of different lines stored in
+** each FDs.*/
 int	get_next_line(const int fd, char **line)
 {
 	static char	*bitem[MAX_FD];
